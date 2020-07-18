2bf7e1d853fa6691a5b91334b960d2cdb6ad5faf
David Feng <davidxifeng@gmail.com>
2020-07-19 00:04:43 +0900

    canvas api simple usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { setPixel, } from './Link'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const canvasRef = React.createRef<HTMLCanvasElement>()
+
+ 	React.useEffect(() => {
+
+		const context2d = canvasRef.current?.getContext('2d')
+		if (!context2d) return
+
+		const imageData = context2d.createImageData(480, 320)
+		for (let i = 0; i < 480 * 320; i++ ) {
+			setPixel(imageData, i, 0, 0, 255, 0, 255)
+		}
+		context2d.putImageData(imageData, 0, 0)
+
+		context2d.fillStyle = 'green'
+		context2d.fillRect(0, 20, 10, 10)
+		context2d.fillRect(470, 310, 10, 10)
+		context2d.font = '48px serif'
+		context2d.strokeStyle = 'red'
+		context2d.fillText("48px serif fill", 0, 60)
+		context2d.font = '36px sans-serif'
+		context2d.strokeText("36px sans-serif; stroke", 0, 120)
+	
+ 	}, [canvasRef])
+
+ 	return (
+		<div style={{
+			margin: 'auto',
+			width: '500px',
+		}}>
+			<canvas
+				ref={canvasRef}
+				width={480} height={320}
+				style={{
+					border: '1px solid black',
+					padding: '1px',
+				}}
+			/>
+		</div>
+	)
 }
 
-export default App;
+export default App
