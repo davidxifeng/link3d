97ec63ba9725f0a96cd445d224e87a781c4f2e60
David Feng <davidxifeng@gmail.com>
2021-03-28 13:39:26 +0900

    move tailwind.css to public folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
-import './styles/tailwind.css'
 
-//ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,
-ReactDOM.render(<App />,
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,
 	document.getElementById('root'));
 serviceWorker.unregister();
